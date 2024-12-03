93934e90be651101ad9ab56109906954eef7e1b1
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-12-02 21:36:49 -0300

    Create hooks, pages and components

--- miniblog/src/App.jsx
+++ miniblog/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
+// pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
+// components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
   return (
     <>
       <div>
-        <h1>Miniblog</h1>
+        <BrowserRouter>
+          <Navbar/>
+            <div className='container'>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>}/>
+              </Routes>
+            </div>
+          <Footer/>
+        </BrowserRouter>
       </div>
     </>
   )
---
+++ miniblog/src/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUMsTA_MXeJ_wqT_peYgaBTF4iNeiYfVQ",
+  authDomain: "miniblog-d6e92.firebaseapp.com",
+  projectId: "miniblog-d6e92",
+  storageBucket: "miniblog-d6e92.firebasestorage.app",
+  messagingSenderId: "161491929957",
+  appId: "1:161491929957:web:3298a97b20538e8eaf2f8c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db}
