efc9f697f60c34bbab963baa5a71369a3bbdb2c1
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2025-01-13 21:07:36 -0300

    update

--- miniblog/src/pages/CreatePost/CreatePost.jsx
+++ miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -3,7 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useAuthentication } from "../../hooks/useAuthentication";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -12,10 +12,46 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
-  const {error, loading } = useAuthentication();
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setFormError("");
+
+    // validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // criar o array de tags
+    const tagsArray = tags.split('.').map((tag)=> tag.trim().toLowerCase())
+
+    // checar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError('Por favor, preencha todos os campos!')
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect home page
+    navigate('/')
+
   };
 
   return (
@@ -66,13 +102,14 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        {!loading && <button className="btn">Criar Post</button>}
-        {loading && (
+        {!response.loading && <button className="btn">Criar Post</button>}
+        {response.loading && (
           <button className="btn" disabled>
             Aguarde...
           </button>
         )}
-        {error && <p className="error">{error}</p>}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
--- miniblog/src/pages/Home/Home.jsx
+++ miniblog/src/pages/Home/Home.jsx
@@ -1,11 +1,50 @@
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
+
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocument";
+
+//components
+import PostDetails from "../../components/PostDetails";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
-export default Home
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>
+          {loading && <p>Carregando...</p> }
+          {posts && posts.map((post) => (
+            <PostDetails key={post.id} post={post}/>
+          ))}
+        </h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
